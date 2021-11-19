6193b6c59f905c401d952203ad2ea1e2d2093d36
Daniel <daniel.ribeiro.nardes2@gmail.com>
2021-11-19 16:35:11 -0300

    função que mostra valores de um objeto no console criada

---
+++ javascript/js/index.js
@@ -0,0 +1,12 @@
+var dados = {
+    nome1:"Daniel",
+    nome2:"Rodrigo",
+    idade1:"16",
+    idade2:"24"
+};
+
+function dados_persoas(){
+    return `${dados.nome1} tem ${dados.idade1}`
+}
+
+console.log(dados_persoas());
